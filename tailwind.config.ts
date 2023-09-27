c025c60cc87163bc75e1c4f088af004e332813a7
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2023-09-27 23:08:14 +0700

    feat: improve sidebar & footer

---
+++ tailwind.config.ts
@@ -0,0 +1,17 @@
+import { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,jsx,ts,tsx,md,mdx}',
+    './components/**/*.{js,jsx,ts,tsx,md,mdx}',
+
+    // Or if using `src` directory:
+    './src/**/*.{js,jsx,ts,tsx,md,mdx}'
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
+
+export default config;
