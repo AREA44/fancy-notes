131fb5b589310d2786653fd914585b80170e815a
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2024-09-29 22:15:52 +0700

    Nextra 3

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,19 +1,12 @@
-import { DocsThemeConfig, useConfig } from 'nextra-theme-docs'
 import { useRouter } from 'next/router'
+import type { DocsThemeConfig } from 'nextra-theme-docs'
+import { useConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   project: {
     link: 'https://github.com/AREA44/fancy-notes',
   },
   docsRepositoryBase: 'https://github.com/AREA44/fancy-notes',
-  useNextSeoProps() {
-    const { asPath } = useRouter()
-    if (asPath !== '/') {
-      return {
-        titleTemplate: '%s | Fancy Notes',
-      }
-    }
-  },
   toc: {
     float: true,
     backToTop: true,
@@ -23,35 +16,31 @@ const config: DocsThemeConfig = {
   },
   logo: <span className="hidden font-bold sm:inline-block">Fancy Notes</span>,
   head: function useHead() {
-    const { title } = useConfig()
+    const config = useConfig()
     const { route } = useRouter()
-    const socialCard =
-      route === '/' || !title
-        ? 'https://fancy-notes.vercel.app/og.svg'
-        : `https://fancy-notes.vercel.app/api/og?title=${title}`
+    const isDefault = route === '/' || !config.title
+    const ogImage =
+      'https://fancy-notes.vercel.app' +
+      (isDefault ? 'og.svg' : `api/og?title=${config.title}`)
+
+    const description =
+      config.frontMatter.description ||
+      'Fancy notes about all kinds of interesting topics.'
+    const title = config.title + (route === '/' ? '' : ' - Fancy Notes')
 
     return (
       <>
+        <title>{title}</title>
+        <meta property="og:title" content={title} />
+        <meta name="description" content={description} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={ogImage} />
+
         <meta name="msapplication-TileColor" content="#fff" />
-        <meta name="theme-color" content="#fff" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta httpEquiv="Content-Language" content="en" />
-        <meta
-          name="description"
-          content="Fancy notes about all kinds of interesting topics."
-        />
-        <meta
-          name="og:description"
-          content="Fancy notes about all kinds of interesting topics."
-        />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:image" content={socialCard} />
+        <meta name="twitter:site:domain" content="Fancy Notes" />
         <meta name="twitter:url" content="https://fancy-notes.vercel.app" />
-        <meta
-          name="og:title"
-          content={title ? title + ' | Fancy Notes' : 'Fancy Notes'}
-        />
-        <meta name="og:image" content={socialCard} />
         <meta name="apple-mobile-web-app-title" content="Fancy Notes" />
         <link rel="icon" href="/favicon.ico" type="image/png" />
         <script
@@ -67,7 +56,7 @@ const config: DocsThemeConfig = {
     next: false,
   },
   footer: {
-    text: (
+    content: (
       <div className="flex justify-between gap-4 w-full text-sm">
         <span>Copyright © {new Date().getFullYear()}</span>
         <p>
