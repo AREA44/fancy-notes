8938d713aec5e9371b91b00dfdf02438dcd616bc
Long Nhat Nguyen <27698189+torn4dom4n@users.noreply.github.com>
2025-02-06 23:29:16 +0700

    chore: enhance metadata and update navbar logo for Fancy Notes branding

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,62 @@
 import { Footer, Layout, Navbar } from "nextra-theme-docs";
+import type { Metadata } from 'next'
 import { Head } from "nextra/components";
 import { getPageMap } from "nextra/page-map";
 import "./globals.css";
 
-export const metadata = {
-  // Define your metadata here
+export const metadata: Metadata = {
   // For more information on metadata API, see: https://nextjs.org/docs/app/building-your-application/optimizing/metadata
-};
+  description: 'Fancy notes about all kinds of interesting topics.',
+  metadataBase: new URL('https://fancy-notes.vercel.app'),
+  generator: 'Next.js',
+  applicationName: 'Fancy Notes',
+  appleWebApp: {
+    title: 'Fancy Notes'
+  },
+  title: {
+    default: 'Fancy Notes - All kinds of interesting topics.',
+    template: '%s | Fancy Notes'
+  },
+  openGraph: {
+    url: './',
+    siteName: 'Fancy Notes',
+    locale: 'en_US',
+    type: 'website'
+  },
+  other: {
+    'msapplication-TileColor': '#fff'
+  },
+  twitter: {
+    site: 'https://fancy-notes.vercel.app'
+  },
+  alternates: {
+    canonical: './'
+  }
+}
+
+const logo = (
+  <span className="hidden font-bold sm:inline-block">Fancy Notes</span>
+)
+
+const footer = <Footer>MIT {new Date().getFullYear()} © AREA44.</Footer>;
+
 
 const navbar = (
   <Navbar
-    logo={<b>Fancy Notes</b>}
-    // ... Your additional navbar options
+    logo={logo}
   />
 );
-const footer = <Footer>MIT {new Date().getFullYear()} © AREA44.</Footer>;
 
 export default async function RootLayout({ children }) {
   return (
     <html lang="en" dir="ltr" suppressHydrationWarning>
-      <Head
-      // ... Your additional head options
-      >
-        {/* Your additional tags should be passed as `children` of `<Head>` element */}
-      </Head>
+      <Head />
       <body>
         <Layout
           navbar={navbar}
           pageMap={await getPageMap()}
           docsRepositoryBase="https://github.com/AREA44/fancy-notes"
           footer={footer}
-          // ... Your additional layout options
         >
           {children}
         </Layout>
