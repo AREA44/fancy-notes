65a700fffbb76c0485b3c3b23cf45ba131a484c1
Long Nhat Nguyen <27698189+torn4dom4n@users.noreply.github.com>
2025-02-07 01:42:38 +0700

    refactor: remove default menu collapse level from sidebar in RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,7 +52,6 @@ export default async function RootLayout({ children }) {
           pageMap={await getPageMap()}
           docsRepositoryBase="https://github.com/AREA44/fancy-notes/tree/main"
           footer={footer}
-          sidebar={{ defaultMenuCollapseLevel: 1 }}
         >
           {children}
         </Layout>
