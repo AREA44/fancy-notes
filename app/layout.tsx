803760c85ad280f6c849460534da1640b095f350
Long Nhat Nguyen <27698189+torn4dom4n@users.noreply.github.com>
2025-02-06 22:23:50 +0700

    chore: update layout components and modify navbar logo for branding

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import { Footer, Layout, Navbar } from "nextra-theme-docs";
-import { Banner, Head } from "nextra/components";
+import { Head } from "nextra/components";
 import { getPageMap } from "nextra/page-map";
 import "nextra-theme-docs/style.css";
 
@@ -8,10 +8,9 @@ export const metadata = {
   // For more information on metadata API, see: https://nextjs.org/docs/app/building-your-application/optimizing/metadata
 };
 
-const banner = <Banner storageKey="some-key">Nextra 4.0 is released 🎉</Banner>;
 const navbar = (
   <Navbar
-    logo={<b>Nextra</b>}
+    logo={<b>Fancy Notes</b>}
     // ... Your additional navbar options
   />
 );
@@ -34,10 +33,9 @@ export default async function RootLayout({ children }) {
       </Head>
       <body>
         <Layout
-          banner={banner}
           navbar={navbar}
           pageMap={await getPageMap()}
-          docsRepositoryBase="https://github.com/shuding/nextra/tree/main/docs"
+          docsRepositoryBase="https://github.com/AREA44/fancy-notes"
           footer={footer}
           // ... Your additional layout options
         >
